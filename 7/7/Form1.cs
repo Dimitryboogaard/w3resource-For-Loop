a2f800e4acbc9ad220fbd6ae204070ad35d4589c
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 20:00:00 +0200

    Add files via upload

---
+++ 7/7/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intTeller2, intInvoer;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt32(tbInvoer.Text);
+
+            for (intTeller2 = 1; intTeller2 <= 10; intTeller2++)
+            {
+                for (intTeller = 1; intTeller <= intInvoer; intTeller++)
+                {
+                    rtUitvoer.Text += intTeller.ToString() + " x " + intTeller2.ToString() +
+                                      " = " + Convert.ToString(intTeller2 * intTeller);
+
+                    if(intTeller < intInvoer)
+                    {
+                        rtUitvoer.Text += ", ";
+                    }
+                }
+
+                rtUitvoer.Text += Environment.NewLine;
+            }
+        }
+    }
+}
---
+++ 8/8/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intSom, intInvoer, intOnevenTeller;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt32(tbInvoer.Text);
+
+            do
+            {
+                intTeller++;
+
+                if (intTeller % 2 != 0)
+                {
+                    tbReeks.Text += intTeller.ToString() + " ";
+                    intSom += intTeller;
+                    intOnevenTeller++;
+                }
+
+            } while (intOnevenTeller < 10);
+
+            tbSom.Text = intSom.ToString();
+        }
+    }
+}
